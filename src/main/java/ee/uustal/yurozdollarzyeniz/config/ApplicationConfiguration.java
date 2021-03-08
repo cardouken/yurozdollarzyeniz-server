d2ad5798e2b52c7e2c8cf97299b68815b113cd02
Cardo Uustal <cardo@onoffapp.com>
2021-03-08 13:31:51 +0200

    add cors mapping to allow from anywhere

--- src/main/java/ee/uustal/yurozdollarzyeniz/config/ApplicationConfiguration.java
+++ src/main/java/ee/uustal/yurozdollarzyeniz/config/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package ee.uustal.yurozdollarzyeniz.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,10 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new MappingJackson2HttpMessageConverter(JsonUtility.getObjectMapper()));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
